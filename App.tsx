3cf255ae0bc0cf2b859f23ea66773b9cd3a645be
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-02-01 22:22:09 -0300

    adding eslint

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { Box, NativeBaseProvider } from 'native-base';
-import { StyleSheet, Text, View } from 'react-native';
+import { Box, NativeBaseProvider } from 'native-base'
 
 export default function App() {
   return (
-   <NativeBaseProvider>
-      <Box>Hello world</Box> 
-   </NativeBaseProvider> 
-  );
+    <NativeBaseProvider>
+      <Box>Hello world</Box>
+    </NativeBaseProvider>
+  )
 }
